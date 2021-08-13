99f37d856255439a215563c81fda626622fea585
Seungmin Lim <seungmin9066@gmail.com>
2021-08-13 17:28:21 +0900

    last edit

--- getaway/views.py
+++ getaway/views.py
@@ -152,15 +152,16 @@ def b_like(request, board_id):
 # ----------------------------- 로긴
 def signup(request):
     if request.method == 'POST':
-        error = {}
         email = request.POST.get('email', None)
         username = request.POST.get('username', None)
         password = request.POST.get('password1', None)
         re_password = request.POST.get('password2', None)
         if not (email and username and password and re_password):
             error = '모든 값을 입력해야 합니다.'
+            return render(request, 'getaway/signup.html', {'error': error})
         elif password != re_password:
             error = '비밀번호가 일치하지 않습니다.'
+            return render(request, 'getaway/signup.html', {'error': error})
         else:
             user = User.objects.create_user(
                 username=request.POST['username'],
@@ -168,8 +169,7 @@ def signup(request):
                 email=request.POST['email'],
                 )
             user.save()
-
-        return render(request, 'getaway/signupcomplete.html')
+            return render(request, 'getaway/signupcomplete.html')
 
     if request.method == 'GET':
         return render(request, 'getaway/signup.html')
@@ -191,7 +191,7 @@ def login(request):
             else:
                 if check_password(password, user.password):
                     request.session['user'] = user.id
-                    return redirect('getaway:b_list')  # 메인페이지
+                    return redirect('/')  # 메인페이지
                 else:
                     error = '비밀번호가 틀렸습니다.'
         return render(request, 'getaway/login.html', {'error': error})
